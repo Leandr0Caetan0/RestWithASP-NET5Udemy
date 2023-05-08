4fefb43ab24ae0b46cd5e554dae183d8f6cb7b2c
unknown <leandro_caetano13@hotmail.com>
2023-05-08 13:49:20 -0300

    Integrando aplicação ao Banco de Dados

--- 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,67 +1,97 @@
-﻿using RestWithASPNETUdemy.Model;
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 
 namespace RestWithASPNETUdemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public List<Person> FindAll()
+        {
+            // SELECT NO BANCO PASSANDO SEM APARAMETRO TRAZENDO TODOS REGISTROS DA TABELA
+            return _context.Persons.ToList();
+        }
+
+        public Person FindByID(long id)
+        {
+            // SELECT NO BANCO PASSANDO O ID COMO PARAMETRO
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
 
         public Person Create(Person person)
         {
+            try
+            {
+                // INSERT NA TABELA PERSON. ATRIBUTOS DO OBJETO SÃO PASSADOS VIA JSON.
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) // VERIFICA SE O ID DA PESSOA NÃO EXISTE. "SE ESSA PESSOAL NÃO EXISTE..."
+            {
+                return new Person(); // CRIA UMA NOVA PESSOA COM OS PARAMETROS PASSADOS VIA JSON
+            }
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id)); // a Variavel result recebe o objeto "antigo"
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person); // compara o objeto antigo com o novo e atualiza os campos diferentes.
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
             return person;
         }
 
         public void Delete(long id)
         {
-            
-        }
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
 
-        public List<Person> FindAll()
-        {
-            List<Person> persons = new List<Person>();
-            for (int i=0; i < 8; i++)
+            if (result != null)
             {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            } 
-            return persons;
-        }
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
 
-        public Person FindByID(long id)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Leandro",
-                LastName = "Caetano",
-                Address = "Rua Granito 57, Londrina - Paraná - Brasil",
-                Gender = "Male"
-            };
+                    throw;
+                }
+            }
         }
-
-        public Person Update(Person person)
+        private bool Exists(long id)
         {
-            return person;
-        }
-
-        private Person MockPerson(int i)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name " + i,
-                LastName = "Person LastName " + i,
-                Address = "Some Address " + i,
-                Gender = "Male"
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
+            // FAZ UM SELECT NA TABELA PERSON PASSANDO O ID COMO PARAMETRO.
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
--- 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Implementations;
 using System;
@@ -28,6 +30,10 @@ namespace RestWithASPNETUdemy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
             //Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImplementation>();
